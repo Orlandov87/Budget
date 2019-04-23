5cc195ea01240dcd47dcd56652aaf79ddfd5c669
Orlando Vargas <OrlandoV87@live.com>
2019-04-22 21:52:06 -0400

    Update

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const { app, BrowserWindow } = require('electron')
 // be closed automatically whne the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, titleBarStyle: 'hidden' })
+  win = new BrowserWindow({ width: 1200, height: 800, titleBarStyle: 'hidden' })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
@@ -17,13 +17,14 @@ function createWindow () {
   // Open the DevTools.
   // win.webContents.openDevTools()
 
-// Emitted when the window is closed.
+  // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference teh window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete thte corresponding element.
     win = null
   })
+
 }
 
 // This method will be called when Electron has finished
@@ -35,7 +36,7 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their emnu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if(process.platform !== 'darwin') {
+  if (process.platform !== 'darwin') {
     app.quit()
   }
 })
