abb1870d7fc607da8c8a19f6708de3be9459d862
Orlando Vargas <OrlandoV87@live.com>
2019-04-29 21:16:56 -0400

    Spelling

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 1200, height: 800, titleBarStyle: 'hidden'})
+  win = new BrowserWindow({ width: 1200, height: 800, titleBarStyle: 'hidden' })
   win.setMenu(null)
 
   // and load the index.html of the app.
@@ -33,7 +33,7 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their emnu bar
+  // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
